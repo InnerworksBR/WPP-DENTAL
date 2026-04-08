877a7e4e39461742bd7e11d71ef0cc37bee7643b
Innerworks <suporte@innerworks.com.br>
2026-04-08 14:10:22 -0300

    Solução: Clean Arquiteture fix

--- src/application/services/__init__.py
+++ src/application/services/__init__.py
@@ -1,18 +1,18 @@
 """Servicos de aplicacao."""
 
 from .appointment_confirmation_service import AppointmentConfirmationService
+from .clean_agent_service import CleanAgentService
 from .conversation_service import ConversationService
 from .conversation_state_service import ConversationState, ConversationStateService
-from .conversation_workflow_service import ConversationWorkflowService
 from .handoff_service import HandoffService
 from .patient_service import PatientService
 
 __all__ = [
     "AppointmentConfirmationService",
+    "CleanAgentService",
     "ConversationService",
     "ConversationState",
     "ConversationStateService",
-    "ConversationWorkflowService",
     "HandoffService",
     "PatientService",
 ]
